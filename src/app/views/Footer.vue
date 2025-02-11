f5ff8acb5229a73bd65d072a49c1ad9a018dc100
LateDream <127290936+LateDreamXD@users.noreply.github.com>
2025-02-12 06:26:00 +0800

    ✨ feat: add debug info

--- src/app/views/Footer.vue
+++ src/app/views/Footer.vue
@@ -1,13 +1,55 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {ref} from 'vue';
+const debugMode = ref<boolean>(import.meta.env.DEV);
+window.debugMode = Object.freeze({
+	on:() => debugMode.value = true,
+	off:() => debugMode.value = false,
+	query:() => debugMode.value
+})
+// debug info
+const appName = __VUE_APP_NAME__;
+const appVersion = __VUE_APP_VERSION__;
+const appEnv = import.meta.env.MODE;
+const viteVersion = __VITE_VERSION__;
+const vueVersion = __VUE_VER__;
+const nodeVersion = __NODE_VER__;
+const v8Version = __V8_VER__;
+const host = location.hostname;
+const devicePlatfrom = navigator.platform;
+</script>
 
 <template>
-	<div id="footer">
+	<div class="pico" id="footer">
+	<div id="footer-main">
 		<span class="ft-item">所有操作均在客户端执行, 请放心使用</span>
 		<span class="ft-item">Made with ❤️ by <a href="https://latedream.us.kg/about/" target="_blank" rel="noopener">LateDream</a></span>
+	</div>
+
+	<details v-if="debugMode" class="debug-info">
+		<summary>🔧 Debug Info<span class="__details-angle">
+			<svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M472.064 272.448l-399.232 399.232c-22.08 22.08-22.08 57.792 0 79.872 22.016 22.016 57.792 22.08 79.872 0L512 392.256l359.296 359.296c22.016 22.016 57.792 22.08 79.872 0 22.08-22.08 22.016-57.792 0-79.872L551.936 272.448C529.856 250.432 494.144 250.432 472.064 272.448z"></path></svg>
+		</span></summary>
+		<span>appName: {{ appName }}</span>
+		<span>appVersion: {{ appVersion }}</span>
+		<span>appEnv: {{ appEnv }}</span>
+		<span>viteVersion: {{ viteVersion }}</span>
+		<span>vueVersion: {{ vueVersion }}</span>
+		<span>nodeVersion: {{ nodeVersion }}</span>
+		<span>v8Version: {{ v8Version }}</span>
+		<span>host: {{ host }}</span>
+		<span>devicePlatfrom: {{ devicePlatfrom }}</span>
+	</details>
 	</div>
 </template>
 
 <style scoped>
+.debug-info {
+	margin-top: 1.2em;
+}
+.debug-info>* {
+	display: block;
+	font-size: 70%;
+}
 #footer {
 	margin-top: 1.4rem;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
+import _package_ from './package.json';
 
 // https://vitejs.dev/config/
 
@@ -7,6 +8,14 @@ export default defineConfig({
 	plugins: [vue()],
 	base: './',
 	define: {
+		__VITE_VERSION__: JSON.stringify(_package_.devDependencies.vite.replace(/[^\d.]/g, '')),
+		__VUE_VER__: JSON.stringify(_package_.dependencies.vue.replace(/[^\d.]/g, '')),
+		__VUE_APP_NAME__: JSON.stringify(process.env.npm_package_name),
 		__VUE_APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+		__NODE_VER__: JSON.stringify(process.version),
+		__V8_VER__: JSON.stringify(process.versions.v8),
 	},
+	server: {
+		host: '0.0.0.0',
+	}
 });
