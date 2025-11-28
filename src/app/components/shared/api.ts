cf676af31ed551a4de999216c8663e6a402a8bd0
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-11-29 06:54:53 +0800

    perf(shared-api): improve copy img

--- src/app/components/shared/api.ts
+++ src/app/components/shared/api.ts
@@ -17,15 +17,6 @@ const genImg = async(target: Node) => {
 	return (await domToPng(target));
 }
 
-const copyImg = async(target: HTMLDivElement, isMobile?: boolean) => {
-	const img = base642blob(await genImg(target));
-	if(isMobile && !confirm('检测到你的设备为移动端, 可能不支持将图片复制到剪贴板.\n尽管如此, 你是否仍要复制图片到剪贴板?'))
-		return;
-	await navigator.clipboard.write([new ClipboardItem({[img.type]: img})])
-		  .then(() => alert('图片已复制到剪贴板!'))
-		  .catch((e: Error) => alert(`复制图片到剪贴板失败: ${e.message}`));
-}
-
 const saveImg = async(target: HTMLDivElement) => {
 	const a = document.createElement('a');
 	a.href = await genImg(target);
@@ -57,5 +48,21 @@ const openDialog = (content?: string | VNode, options?: {
 	return dialog;
 }
 
-export {file2base64, base642blob, changeImgs, genImg, copyImg, saveImg, openDialog};
+const wirteBlob2Clipboard = async(blob: Blob) => await navigator.clipboard.write([new ClipboardItem({[blob.type]: blob})]);
+
+const copyImg = async(target: HTMLDivElement, isMobile?: boolean) => {
+	const img = base642blob(await genImg(target));
+	if(isMobile) openDialog('检测到你的设备为移动端, 可能不支持将图片复制到剪贴板.\n尽管如此, 你是否仍要复制图片到剪贴板?', {
+		confirmCallback: async() => {
+			await wirteBlob2Clipboard(img)
+				  .then(() => openDialog('图片已复制到剪贴板!'))
+				  .catch((e: Error) => openDialog(`复制图片到剪贴板失败: ${e.message}`));
+		}
+	});
+	else await wirteBlob2Clipboard(img)
+			   .then(() => openDialog('图片已复制到剪贴板!'))
+			   .catch((e: Error) => openDialog(`复制图片到剪贴板失败: ${e.message}`));
+}
+
+export {file2base64, base642blob, changeImgs, genImg, copyImg, saveImg, openDialog, wirteBlob2Clipboard};
 export default {changeImgs, copyImg, saveImg};
